8bd98786d299537666017448999cb9622bb8218e
manato kuroda <a7108100@gmail.com>
2022-12-09 16:18:47 +0900

    feat: add countries page

--- src/shared/apollo/createHttpLink.ts
+++ src/shared/apollo/createHttpLink.ts
@@ -1,32 +1,15 @@
-import { split, HttpLink } from '@apollo/client'
-import { getMainDefinition } from '@apollo/client/utilities'
+import { HttpLink } from '@apollo/client'
 import { config } from 'src/config'
-import { isClient } from 'src/shared/environment'
 
 export type CreateHttpProps = {
   idToken: string
 }
 
 export const createHttpLink = (props: CreateHttpProps) => {
-  const httpLink = new HttpLink({
+  return new HttpLink({
     uri: config.API_URL,
     headers: {
       Authorization: `Bearer ${props.idToken}`,
     },
   })
-
-  if (isClient()) {
-    return split(
-      ({ query }) => {
-        const definition = getMainDefinition(query)
-        return (
-          definition.kind === 'OperationDefinition' &&
-          definition.operation === 'subscription'
-        )
-      },
-      httpLink,
-    )
-  }
-
-  return httpLink
 }
